4af617ba6336f439c4f4da5ba7297c2b310a44d4
SCU02-NB227\user <mk.tri.setiawan2@pertamina.com>
2025-07-14 20:58:13 +0700

    kasir modul

---
+++ dashboard/kasir/_component/sidebar.php
@@ -0,0 +1,40 @@
+    <nav class="pc-sidebar">
+        <div class="navbar-wrapper">
+            <div class="m-header flex items-center py-4 px-6 h-header-height">
+                <a href="../../dashboard/index.html" class="b-brand flex items-center gap-3">
+                    <!-- ========   Change your logo from here   ============ -->
+                    <img src="../../../../assets/images/logo-white.svg" class="img-fluid logo logo-lg" alt="logo" />
+                    <img src="../../assets/images/favicon.svg" class="img-fluid logo logo-sm" alt="logo" />
+                </a>
+            </div>
+            <div class="navbar-content h-[calc(100vh_-_74px)] py-2.5">
+                <ul class="pc-navbar">
+                    <li class="pc-item pc-caption">
+                        <label>Dashboard Portal</label>
+                    </li>
+                    <li class="pc-item">
+                    <li class="pc-item">
+                        <a href="index.php" class="pc-link">
+                            <span class="pc-micon">
+                                <i data-feather="home"></i>
+                            </span>
+                            <span class="pc-mtext">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="pc-item pc-caption">
+                        <label>Transaction Data</label>
+                        <i data-feather="feather"></i>
+                    </li>
+                    <li class="pc-item pc-hasmenu">
+                        <a href="#!" class="pc-link"><span class="pc-micon"> <i data-feather="shopping-cart"></i> </span><span
+                                class="pc-mtext">Data Booking</span><span class="pc-arrow"><i class="ti ti-chevron-right"></i></span></a>
+                        <ul class="pc-submenu">
+                            <li class="pc-item"><a class="pc-link" href="aktif.php">Aktif</a></li>
+                            <li class="pc-item"><a class="pc-link" href="nonaktif.php">Non Aktif</a></li>
+                            <li class="pc-item"><a class="pc-link" href="histori.php">Histori</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
